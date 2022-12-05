125093bd05818dbb4dbcbd9b2df6f279a0212b39
omer0436 <111450831+omer0436@users.noreply.github.com>
2022-12-06 00:55:05 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,10 +1,31 @@
-const getProductsOfNormalQuality = () => {};
+const getProductsOfNormalQuality = (newproductList) => {
+  return newproductList.filter((product) => product.quality === "Normal");
+};
 
-const getProductImageWithPngExtension = () => {};
+const getProductImageWithPngExtension = (newproductList) => {
+  const result = newproductList.filter(
+    (product) => product.productImage.at(-2) === "n"
+  );
 
-const getCalorieOfTheMostExpensiveProduct = () => {};
+  return result;
+};
 
-const sortByExpirationDate = () => {};
+const getCalorieOfTheMostExpensiveProduct = (newproductList) => {
+  const biggestPrice = Math.max(
+    ...newproductList.map((pArray) => pArray.price)
+  );
+
+  return newproductList.find((pArray) => pArray.price === biggestPrice)
+    .totalCalories;
+};
+
+let sortByExpirationDate;
+
+sortByExpirationDate = (productList) => {
+  return productList.sort(
+    (a, b) => new Date(a.expireDate) - new Date(b.expireDate)
+  );
+};
 
 export {
   getProductsOfNormalQuality,
